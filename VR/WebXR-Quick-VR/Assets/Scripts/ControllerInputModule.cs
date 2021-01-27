1c82aeaab8506158048df5f50610416ab14ee321
De-Panther <depanther@gmail.com>
2021-01-27 12:15:26 +0200

    UI Canvas + ControllerInputModule

---
+++ VR/WebXR-Quick-VR/Assets/Scripts/ControllerInputModule.cs
@@ -0,0 +1,73 @@
+﻿// Controller Input Module by De-Panther
+// Based on Peter Koch <peterept@gmail.com> Gaze Input Module
+// http://talesfromtherift.com/vr-gaze-input/
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using WebXR;
+
+public class ControllerInputModule : PointerInputModule 
+{
+  public WebXRController controller;
+  public Transform hitPoint;
+  public GameObject hitPointGameObject;
+
+	private PointerEventData pointerEventData;
+	private GameObject currentPointAtHandler;
+
+	public override void Process()
+	{ 
+		HandlePoint();
+		HandleSelection();
+	}
+
+	void HandlePoint()
+	{
+		if (pointerEventData == null)
+		{
+			pointerEventData = new PointerEventData(eventSystem);
+		}
+		if (UnityEngine.XR.XRSettings.enabled)
+		{
+			pointerEventData.position = new Vector2(UnityEngine.XR.XRSettings.eyeTextureWidth * 0.5f, UnityEngine.XR.XRSettings.eyeTextureHeight * 0.5f);
+		}
+		else
+		{
+			pointerEventData.position = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+		}
+		pointerEventData.delta = Vector2.zero;
+    List<RaycastResult> raycastResults = new List<RaycastResult>();
+    eventSystem.RaycastAll(pointerEventData, raycastResults);
+    pointerEventData.pointerCurrentRaycast = FindFirstRaycast(raycastResults);
+    hitPointGameObject.SetActive(pointerEventData.pointerCurrentRaycast.isValid);
+    if (pointerEventData.pointerCurrentRaycast.isValid)
+    {
+      hitPoint.position = pointerEventData.pointerCurrentRaycast.worldPosition;
+    }
+		ProcessMove(pointerEventData);
+	}
+
+	void HandleSelection()
+	{
+		if (pointerEventData.pointerEnter != null)
+		{
+			GameObject handler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(pointerEventData.pointerEnter);
+			if (currentPointAtHandler != handler)
+			{
+				currentPointAtHandler = handler;
+			}
+
+			if (currentPointAtHandler != null && controller.GetButtonUp(WebXRController.ButtonTypes.Trigger))
+			{
+				ExecuteEvents.ExecuteHierarchy(currentPointAtHandler, pointerEventData, ExecuteEvents.pointerClickHandler);
+			}
+		}
+		else
+		{
+			currentPointAtHandler = null;
+		}
+	}
+
+
+}
---
+++ VR/WebXR-Quick-VR/Assets/Scripts/SwitchCanvasInputModule.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using WebXR;
+
+public class SwitchCanvasInputModule : MonoBehaviour
+{
+  public WebXRController controller;
+  public GameObject controllerRay;
+  public Camera mainCamera;
+  public Camera controllerCamera;
+  public ControllerInputModule controllerInputModule;
+  public StandaloneInputModule standaloneInputModule;
+  public Canvas canvas;
+
+  public void Awake()
+  {
+    HandleControllerActive(false);
+  }
+
+  private void OnEnable()
+  {
+    if (controller.isControllerActive)
+    {
+      HandleControllerActive(true);
+    }
+    controller.OnControllerActive += HandleControllerActive;
+  }
+
+  private void OnDisable()
+  {
+    controller.OnControllerActive -= HandleControllerActive;
+  }
+  
+  private void HandleControllerActive(bool active)
+  {
+    if (active)
+    {
+      controllerInputModule.enabled = true;
+      standaloneInputModule.enabled = false;
+      canvas.worldCamera = controllerCamera;
+      controllerRay.SetActive(true);
+    }
+    else
+    {
+      controllerInputModule.enabled = false;
+      standaloneInputModule.enabled = true;
+      canvas.worldCamera = mainCamera;
+      controllerRay.SetActive(false);
+    }
+  }
+}
